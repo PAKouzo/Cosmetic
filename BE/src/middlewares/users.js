c59495e0237370638fc3aeceb3ed8fe47bda4ddd
tobacoguys <dungchelsea1723@gmail.com>
2024-07-27 19:06:39 +0700

    update API user login

--- BE/src/middlewares/users.js
+++ BE/src/middlewares/users.js
@@ -25,6 +25,30 @@ const UserMDW = {
             status: "Failed!",
         })
     }},
+
+    checkLogin: async(req, res, next)=>{
+        const { email, password } = req.body;
+        try{
+            const user = await UserModel.findOne({ email }); //Hàm findOne(trong Moongoose) dùng để kiểm tra xem có người dùng nào xác thực trong CSDL hay không
+            if (!user) {
+                throw new Error ("Incorrect email");
+            }
+            else{
+                const verifyPassword = await bcrypt.compare( password, user.password) //dùng hàm compare để so sánh mật khẩu người dùng nhập với mật khẩu trên CSDL
+                if(!verifyPassword){
+                    throw new Error ("Incorrect password");
+                }
+            }
+            
+            next();
+        }
+        catch(e){
+            return res.status(404).send({
+                message: e,
+                status: "Unsuccessful",
+                data: null,
+            })
+        }},
 }
 
 export default UserMDW;
